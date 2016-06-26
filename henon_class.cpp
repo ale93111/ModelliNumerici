707de01618e6e2d4cd88f8fb368a2c683a0983cc
Alessandro Marzo <alessandrom032@gmail.com>
2016-06-26 17:17:44 +0200

    usando newton

--- henon_class.cpp
+++ henon_class.cpp
@@ -3,8 +3,11 @@
 #include <vector>
 #include <random>
 #include <algorithm> // std::max
-#include <math.h>    // sqrt
+#include <math.h>	// sqrt
 #include <complex> //std::sqrt
+
+#include"fourier.h"
+#include"newton.h"
 
 #define npoints 1000
 #define PI 3.14159265359
@@ -15,12 +18,19 @@ std::normal_distribution<double> gauss_distr(0.0, 1.0);
 std::uniform_real_distribution<double> uniform_distr(-1, 1);
 
 
+void pol(double x, double *func, double *dfunc, const double a, const double b, const double c)
+{
+	*func  = a - b*x*x + c*x*x*x;
+	*dfunc =  - 2.0*b*x + 3.0*c*x*x;
+}
+
+
 struct Ensemble
 {
-	double *p, *q, *E, *action, *period;
+	double *p, *q, *E, *action, *dI_dE;
 	double w, k, epsilon, t;
 	int Nparticles;
-	
+	void (*pfpolinomio)(double, double *, double *, const double , const double , const double ); 	
 	
 	double avg_energy()
 	{
@@ -71,6 +81,11 @@ struct Ensemble
 	
 		root1 = croot1.real();
 		root2 = croot2.real();
+		
+		//root1 = rtsafe(pfpolinomio, root1 - 0.01*root1, root1 + 0.01*root1, 1e-6, a, b, c);
+		//root2 = rtsafe(pfpolinomio, root2 - 0.01*root2, root2 + 0.01*root2, 1e-6, a, b, c);
+		
+		std::cout << "root1 = " << root1 << "  root2 = " << root2 << std::endl;
 	}
 	
 	
@@ -89,8 +104,8 @@ struct Ensemble
 	double tex(const double & E, const double & x) //action integrando p(E,x)
 	{
 		//if((2.0*E - w*w*x*x + (2.0/3.0)*k*x*x*x) < 0.0) std::cout << "errore " << 2.0*E - w*w*x*x + (2.0/3.0)*k*x*x*x <<std::endl <<std::flush;
-		double temp = 1.0/(2.0*E - w*w*x*x + (2.0/3.0)*k*x*x*x);
-		return sqrt( temp<1e-10? 0.0 : temp );
+		double temp = (2.0*E - w*w*x*x + (2.0/3.0)*k*x*x*x);
+		return sqrt( temp<1e-10? 0.0 : 1.0/temp );
 	}
 	
 	double action_simpson(double E, double a, double b, int N)
@@ -109,10 +124,10 @@ struct Ensemble
 		
 		sum *= dx/6.0;
 			
-		return 2.0*sum;
+		return sum/PI;
 	}
 	
-	double period_simpson(double E, double a, double b, int N)
+	double dI_dE_simpson(double E, double a, double b, int N)
 	{
 		// usare la funzione pex può restituire dei nan perché la funzione find root
 		// approssima i valori numerici degli zeri di pex che è una sqrt quindi può
@@ -128,7 +143,7 @@ struct Ensemble
 		
 		sum *= dx/6.0;
 			
-		return 2.0*sum;
+		return sum/PI;
 	}
 	
 	void symplectic_advance(double & q, double & p, const double dt, const double t)
@@ -157,14 +172,23 @@ struct Ensemble
 			find_roots(root1, root2, 2.0*E[i], w*w, 2.0*k/3.0);		
 			
 			action[i] = action_simpson(E[i], root1, root2, npoints);
-			//period[i] = period_simpson(E[i], root1, root2, npoints);
+			dI_dE[i] = dI_dE_simpson(E[i], root1, root2, npoints);
 		}
 	}
 	
-		
-	Ensemble(){}
+	Ensemble(){}	
+	Ensemble( int N )
+	{
+		p = new double[N];
+		q = new double[N];
+		E = new double[N];
+		action = new double[N];
+		dI_dE = new double[N];	
+	}
+	
 	Ensemble(int N, double wi, double ki, double epsiloni, double Ei)
 	{
+		pfpolinomio = pol;
 		Nparticles = N;
 		w = wi;
 		k = ki;
@@ -174,16 +198,16 @@ struct Ensemble
 		q = new double[N];
 		E = new double[N];
 		action = new double[N];
-		period = new double[N];
+		dI_dE = new double[N];
 		
 		t = 0.0;
 		
 		double root1, root2;
 		
 		find_roots(root1, root2, 2.0*Ei, w*w, 2.0*k/3.0);
-
+				std::cout << "ciao sono il costruttore" << std::endl;
 		double actioni = action_simpson(Ei, root1, root2, npoints);
-		double periodi = period_simpson(Ei, root1, root2, npoints);
+		double dI_dEi = dI_dE_simpson(Ei, root1, root2, npoints);
 		
 		for(int i=0; i<N; i++) 
 		{
@@ -191,11 +215,11 @@ struct Ensemble
 			q[i] = 0.0;
 			p[i] = pex(Ei, q[i]);
 			action[i] = actioni;
-			period[i] = periodi;
+			dI_dE[i] = dI_dEi;
 		}
 	}	
 	
-	Ensemble( const Ensemble& other ) : Ensemble(other.Nparticles, other.w, other.k, other.epsilon, other.E[0])
+	Ensemble( const Ensemble& other ) : Ensemble( other.Nparticles )
 	{
 		w = other.w;
 		k = other.k;
@@ -209,13 +233,13 @@ struct Ensemble
 			q[i] = other.q[i];
 			p[i] = other.p[i];
 			action[i] = other.action[i];
-			period[i] = other.period[i];
+			dI_dE[i] = other.dI_dE[i];
 		}
 	}
 	
 	~Ensemble()	
 	{
-		delete p, q, E, action, period;	
+		delete p, q, E, action, dI_dE;	
 	}
 };
 
@@ -225,28 +249,31 @@ double slope_linear_regression(const std::vector<double>& x, const std::vector<d
 	int n = x.size();
 	
 	double avgX = accumulate(x.begin(), x.end(), 0.0) / n;
-    double avgY = accumulate(y.begin(), y.end(), 0.0) / n;
+	double avgY = accumulate(y.begin(), y.end(), 0.0) / n;
 	
 	double Sxy = 0.0;
-    double Sxx = 0.0;
+	double Sxx = 0.0;
 
-    for(int i=0; i<n; ++i){
-        Sxy += (x[i] - avgX) * (y[i] - avgY);
-        Sxx += (x[i] - avgX) * (x[i] - avgX);
-    }
+	for(int i=0; i<n; ++i){
+		Sxy += (x[i] - avgX) * (y[i] - avgY);
+		Sxx += (x[i] - avgX) * (x[i] - avgX);
+	}
 
-    return Sxy / Sxx;
+	return Sxy / Sxx;
 }
+
 
 int main(int argc, char *argv[])
 {
 
-	int Nensemble = 1000;
+	int Nensemble = 1;
 	int nsteps = 10;
+	int Ndynamic = 512;
 	
 	double w = 1.0;
 	double k = 1.0;
 	double dt = 0.01;
+	double dtdynamic = 0.001;
 	double epsilon = 0.1;
 	
 	double E = 0.1;
@@ -255,15 +282,20 @@ int main(int argc, char *argv[])
 	std::cout << "Emax = " << Emax << std::endl;
 	std::cout << std::endl;
 
-	double coeff_drift, coeff_diffusion;
+	double coeff_drift, coeff_diffusion, coeff_diffusion_theoretical;
 	std::vector<double> Ncoeff_diffusion, Ntime;
+	std::vector<double> Ncoeff_diffusion_dynamic;
+	
+	double *q_p_f;
+	q_p_f = new double[Ndynamic*Nensemble];
+	
 	
 	Ensemble ensemble(Nensemble, w, k, epsilon, E);
 	
 	Ensemble ensemble_0 = ensemble;
 	
 	std::cout << "t iniziale = " << ensemble.t << "\t" << "E[0] iniziale = " << ensemble.E[0] << std::endl
-			  << "azione[0] iniziale = " << ensemble.action[0] << "\t" << "period[0] iniziale = " << ensemble.period[0] << std::endl;
+			  << "azione[0] iniziale = " << ensemble.action[0] << "\t" << "dI_dE[0] iniziale = " << ensemble.dI_dE[0] << std::endl;
 	
 	double avg_action_0 = ensemble.avg_action();
 	
@@ -273,24 +305,56 @@ int main(int argc, char *argv[])
 		
 		Ntime.push_back(ensemble.t);
 		Ncoeff_diffusion.push_back( ensemble.avg_action_for_diffusion(ensemble_0)/dt ); 
-		//Ncoeff_diffusion.push_back( pow( ensemble.avg_action() - avg_action_0 , 2.0)/dt ); //check formula of average values
+		
+		Ensemble ensemble_temp = ensemble;
+		//propaga la dinamica simplettica per Ndynamic passi
+		for(int j=0; j<Ndynamic; j++)
+		{
+			//per ogni particella
+			for(int n=0; n<ensemble_temp.Nparticles; n++)
+			{
+				ensemble_temp.symplectic_advance( ensemble_temp.q[n], ensemble_temp.p[n], dtdynamic, ensemble_temp.t);
+				
+				//costruisco un segnale di cui vado a fare la FFT
+				q_p_f[n*Ndynamic + j] = pow( ensemble_temp.q[n]*ensemble_temp.p[n]/ensemble_temp.dI_dE[n] , 2.0); // ;
+			}
+			
+			ensemble_temp.t += dtdynamic;
+		}
+		
+		double avg_diffusion_temp = 0.0;
+		
+		for(int n=0; n<ensemble_temp.Nparticles; n++)
+		{
+			//FFT per la traiettoria di ogni particella
+			realft( &q_p_f[n*Ndynamic], Ndynamic );
+			
+			avg_diffusion_temp += q_p_f[n*Ndynamic];	
+			//avg_diffusion_temp += sqrt(q_p_f[n*Ndynamic]*q_p_f[n*Ndynamic] + q_p_f[n*Ndynamic + 1]*q_p_f[n*Ndynamic + 1]) / Ndynamic;	
+		}
+		avg_diffusion_temp /= Ndynamic*ensemble_temp.Nparticles;
+		Ncoeff_diffusion_dynamic.push_back( epsilon*epsilon*avg_diffusion_temp );		
 	}
 	
 	coeff_drift = (ensemble.avg_action() - avg_action_0)/dt;
 	coeff_diffusion = slope_linear_regression(Ntime, Ncoeff_diffusion);
-	
-	
+	//coeff_diffusion_theoretical = slope_linear_regression(Ntime, Ncoeff_diffusion_dynamic);
+	coeff_diffusion_theoretical = accumulate(Ncoeff_diffusion_dynamic.begin(), Ncoeff_diffusion_dynamic.end(), 0.0) / Ncoeff_diffusion_dynamic.size();
+		
+		
 	std::cout << "t finale = " << ensemble.t << "\t" << "E[0] finale = " << ensemble.E[0] << std::endl
-			  << "azione[0] finale = " << ensemble.action[0] << "\t" << "period[0] finale = " << ensemble.period[0]<< std::endl;
+			  << "azione[0] finale = " << ensemble.action[0] << "\t" << "dI_dE[0] finale = " << ensemble.dI_dE[0]<< std::endl;
 	
 	
 	
 	std::cout << std::endl << std::endl;
 	std::cout << "coeff_drift = " << coeff_drift << std::endl;
 	std::cout << "coeff_diffusion = " << coeff_diffusion << std::endl;
+	std::cout << "coeff_diffusion_theoretical = " << coeff_diffusion_theoretical << std::endl;
 	
-	//for(int i=0; i<Ncoeff_diffusion.size(); i++) std::cout << Ncoeff_diffusion[i] << std::endl;
-	
+	//for(int i=0; i<Ncoeff_diffusion.size(); i++) 		 std::cout << Ncoeff_diffusion[i] 		  << std::endl;
+	std::cout << std::endl;
+	//for(int i=0; i<Ncoeff_diffusion_dynamic.size(); i++) std::cout << Ncoeff_diffusion_dynamic[i] << std::endl;
 	
 	/*
 	std::ofstream output;	
